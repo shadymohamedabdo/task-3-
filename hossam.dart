a6c706f6a8aaf5dd38c64747f810fdc0369b5df2
Shady Mohamed <142973431+shadymohamedabdo@users.noreply.github.com>
2023-08-27 10:27:07 +0300

    Update hossam.dart

--- hossam.dart
+++ hossam.dart
@@ -2,7 +2,7 @@
 import 'dart:io';
 import 'package:hossam/hossam.dart' as hossam;
 void main() {
-  /// task 4 (Create a program that asks the user for a number and then prints out a list of all the divisors of that number)
+  /// task 3 (Create a program that asks the user for a number and then prints out a list of all the divisors of that number)
 
   stdout.write('please choose a number : ');
   String? x = stdin.readLineSync();
