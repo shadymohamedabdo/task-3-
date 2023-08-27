f01deae1b3b64ea42c55c38a9735e6f8cf9cdaf8
Shady Mohamed <142973431+shadymohamedabdo@users.noreply.github.com>
2023-08-27 10:04:49 +0300

    Add files via upload

---
+++ hossam.dart
@@ -0,0 +1,18 @@
+
+import 'dart:io';
+import 'package:hossam/hossam.dart' as hossam;
+void main() {
+  /// task 3 (Take a list and write a program that prints out all the elements of the list that are less than 5)
+
+  List <int> x = [3,2,4,7,9,1,4,8];
+  for (var i in x ){
+    if (i<5)
+    print(i);
+}
+  }
+
+
+
+
+
+
